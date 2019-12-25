130af0942fb3049b757520b2f2d1c6cd629342b4
Alex Olieman <alex@olieman.net>
2019-12-25 01:58:51 +0100

    license as Apache 2.0

--- new_batches.py
+++ new_batches.py
@@ -182,3 +182,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
     update_index_state(args.index_filter)
     get_new_batches(args.index_filter, args.revisit)
+
+"""
+    Copyright 2019 Alex Olieman <alex@olieman.net>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
--- wordhoard_helpers.py
+++ wordhoard_helpers.py
@@ -90,3 +90,20 @@ def post_wordhoard_payload(item_id, rdf_type, topics, wordhoard_id=None, wh_type
         else:
             print("Wordhoard create POST error...", file=sys.stderr)
         return response.status_code, response.text
+
+
+"""
+    Copyright 2019 Hendrik Grondijs, Alex Olieman <alex@olieman.net>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
